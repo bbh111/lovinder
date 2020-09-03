e8caa508ff2d4ea5f7306ceea5fe86592a8a8193
= <=>
2020-09-03 16:12:13 +0700

    new project

---
+++ src/main/java/com/company/lovinder/LovinderApplication.java
@@ -0,0 +1,13 @@
+package com.company.lovinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/lovinder/LovinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.lovinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
